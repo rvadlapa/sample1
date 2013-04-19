ea2fbcc99937a61d941ffb2880a28056193b45ea
ciuser <rvadlapa@gmail.com>
2013-04-19 16:33:11 -0700

    one out

--- src/com/ramesh/sample/Sample.java
+++ src/com/ramesh/sample/Sample.java
@@ -6,6 +6,5 @@ public class Sample {
  public static void main(String[] args) {
   System.out.println("hi---I wor...k");
   System.out.println("hi---I wor...k");
-  System.out.println("hi---I wor...k");
  }
 }
