78ca5be23a787e6a111e5b35fd6a8dfa115a7b60
ciuser <rvadlapa@gmail.com>
2013-05-05 21:03:38 -0700

    added more code

--- src/com/ramesh/sample/Sample.java
+++ src/com/ramesh/sample/Sample.java
@@ -3,7 +3,11 @@ import java.net.*;
 
 public class Sample {
  public static void main(String[] args) {
-  System.out.println("hi---I wor...k");
-  System.out.println("hi---I wor...k");
+  System.out.println(".....Program Starts Here.....");
+  ArrayList aList = new ArrayList();
+  for(int i=0; i<10; i++) { 
+   aList.add("Test " + i);
+  }
+  System.out.println(".....Program Ends Here.....");
  }
 }
