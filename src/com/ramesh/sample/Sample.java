b11a02ddf3989388864f4ba96382414aaa28e591
ciuser <rvadlapa@gmail.com>
2013-05-06 13:03:47 -0700

    done

--- src/com/ramesh/sample/Sample.java
+++ src/com/ramesh/sample/Sample.java
@@ -8,7 +8,7 @@ public class Sample {
   for(int i=0; i<10; i++) { 
    aList.add("Test " + i);
   }
-  System.out.println(aList.size());
+  System.out.println(aList.size() + " Size: ");
   System.out.println(".....Program Ends Here.....");
  }
 }
